8c17c3ea73589f00ef73d22e9b149f3b077002cb
Grinch Is Me <demyanov0808@gmail.com>
2021-10-07 14:05:34 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+     
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
